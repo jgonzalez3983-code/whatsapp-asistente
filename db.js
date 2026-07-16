3618f703875b854c57768890b8aafe3120a5b332
jgonzalez3983-code <jgonzalez3983@gmail.com>
2026-07-15 23:39:27 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'datos.db'));
+const fs = require('fs');
+const carpetaDatos = fs.existsSync('/data') ? '/data' : __dirname;
+const db = new Database(path.join(carpetaDatos, 'datos.db'));
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS items (
